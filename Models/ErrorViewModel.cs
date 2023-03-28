76c820abc8fca74b66a5248ca52b31ae3902d24e
Jackson Collins <jwcollins3@crimson.ua.edu>
2023-03-28 11:57:40 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _521Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
