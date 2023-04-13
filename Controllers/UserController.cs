3de2b15772b914c65f7e672076249e0873c26373
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-04-13 13:44:26 -0500

    frog and carousel

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _521Final.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult MyBooks()
+        {
+            return View();
+        }
+    }
+}
