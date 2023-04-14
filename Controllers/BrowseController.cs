6924d745a80b88b5cf1a61bd1b3228c8e8dc7ff5
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-04-14 12:04:56 -0500

    Updated a few things

---
+++ Controllers/BrowseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _521Final.Controllers
+{
+    public class BrowseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
