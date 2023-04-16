5781cb9550cb197cc1f2b2b9a27f3d325f388928
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-04-16 17:40:49 -0500

    changed things in controllers

--- Controllers/BrowseController.cs
+++ Controllers/BrowseController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _521Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace _521Final.Controllers
 {
     public class BrowseController : Controller
     {
+        private readonly ILogger<BrowseController> _logger;
+
+        public BrowseController(ILogger<BrowseController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
