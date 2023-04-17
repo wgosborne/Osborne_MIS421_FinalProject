f7058fe476b44ad78863398da4711c2cdfa6537a
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-04-16 23:05:34 -0500

    tried to fix the mybooks html. Still looks uggo. Sorry

--- wwwroot/js/MyBooks.js
+++ wwwroot/js/MyBooks.js
@@ -16,3 +16,21 @@ $(document).ready(function () {
         }
     });
 });
+
+//rendering the book meta data into the box, probably gotta pass in the currBook UNCOMMENT THIS OUT ONCE WE HAVE THE BOOKS
+function RenderBookMetaData() {
+
+    //Select the HTML element where we want to render the book information
+    const bookData = document.querySelector('#BookMetaData');
+
+    //Creating an HTML template with placeholders for the book properties, gonna need to add a try catch to see if they've rated/finished it
+    const html = `
+      <p>Date Started: ${book.DateStarted}          Date Finished: ${book.DateFinished}</p>
+      <p>Your Rating: ${book.Rating}          Average Rating: ${book.AvRating}</p>
+      <p>Similar Reads: ${book.Similar}</p>
+      <p>Your Review: ${book.MyReview}</p>
+    `;
+
+    //Setting the HTML
+    bookData.innerHTML = html;
+}
