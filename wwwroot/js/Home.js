9a4fcaf07357c384285df8443c44e84269ad5fbb
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-04-17 20:49:21 -0500

    changed await and chat gpt stuff

--- wwwroot/js/Home.js
+++ wwwroot/js/Home.js
@@ -1,9 +1,12 @@
-﻿function GetBookSummary() {
+﻿
+function GetBookSummary() {
+
+    //ChatGPTClient chat = new ChatGPTClient
 
     var searchBook = document.getElementById("BookSummaryInput");
 
     try {
-        var summary = await ChatGPTClient.GenerateSummary(searchBook);
+        var summary = ChatGPTClient.GenerateSummary(searchBook);
         console.log(summary);
         //Console.WriteLine($"Summary of {bookName}: {summary}");
         document.getElementById("BookSummaryArea").innerHTML = "<p " + summary + "</p>";
