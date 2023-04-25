0391d63f65a5245e879a94fd90d7f9d5af7022ba
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-04-25 11:28:24 -0500

    worked on the create page

--- Data/Migrations/20230425154041_forAddingBooks2.Designer.cs
+++ Data/Migrations/20230425154041_forAddingBooks2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _521Final.Data;
 
@@ -11,9 +12,10 @@ using _521Final.Data;
 namespace _521Final.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230425154041_forAddingBooks2")]
+    partial class forAddingBooks2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
