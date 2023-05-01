117e6e7d1f8c4aa7e52ef8fedeacc63438fd4abe
Emma Isbell <eaisbell@crimson.ua.edu>
2023-05-01 14:23:26 -0500

    trying to fix book photo

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,13 +6,13 @@ public class Book //will get filled in with Azure Books data, we can fill these 
 {
 	public int Id { get; set; }
 	[Url]
+
 	public string? HyperLink { get; set; } //Link to the book on the site, so they can click on it
 	//public DateTime? StartDate { get; set; }
 
 	//public DateTime? EndDate { get; set; }
 
 	//public double? userRating { get; set; }
-
 	public string? Title { get; set; }
 
 	public string? Author { get; set; }
